a309d669fdb368911476ba6f863cb147214df8f0
mdbow22 <mdbow22@gmail.com>
2021-10-24 13:45:50 -0400

    added get routes for Categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,41 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findAll(
+      { include: [{model: Product}] });
+  
+    if(allCategories) {
+      res.status(200).json(allCategories);
+    } else {
+      res.status(400).json('No Data found');
+    }
+  } catch (err) {
+      res.status(500).json(err);
+  }
+  
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const thisCategory = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+
+    if(thisCategory) {
+      res.status(200).json(thisCategory);
+    } else {
+      res.status(400).json('Category does not exist');
+    }
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
