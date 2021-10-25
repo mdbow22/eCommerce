33bcaf0d2793c85e944e2b530db4676462165ad6
mdbow22 <mdbow22@gmail.com>
2021-10-24 21:38:41 -0400

    finished routes for categories table

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,22 +34,67 @@ router.get('/:id', async (req, res) => {
     } else {
       res.status(400).json('Category does not exist');
     }
-    
+
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    if(req.body.category_name) {
+      const newCategory = await Category.create({ category_name: req.body.category_name });
+      res.status(200).json(newCategory);
+    } else {
+      res.status(400).json('Invalid Request');
+    }
+  } catch (err) {
+    res.status(500).json('Unable to create record');
+  }
+  
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const change = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    if(change[0] === 0) {
+      res.status(400).json('Invalid PUT request');
+    }
+
+    res.status(200).json(change);
+
+    }
+  catch (err) {
+    res.status(500).json('Unable to update record');
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryDel = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if(categoryDel === 0) {
+      res.status(400).json('Unable to delete record');
+    }
+
+    res.status(200).json('Category deleted successfully');
+    
+  } catch (err) {
+    res.status(500).json('Could not process request')
+  }
+  
 });
 
 module.exports = router;
