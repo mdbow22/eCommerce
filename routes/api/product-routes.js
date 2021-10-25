7507fc06a1559da40c6217af4fa7dca20030e5c7
mdbow22 <mdbow22@gmail.com>
2021-10-25 14:21:35 -0400

    created functionality to request single product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const allProducts = await Product.findAll(
+      { include: [{model: Category},{model: Tag}] });
+  
+    if(allProducts) {
+      res.status(200).json(allProducts);
+    } else {
+      res.status(400).json('No Data found');
+    }
+  } catch (err) {
+      res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const singleProduct = await Product.findByPk(req.params.id,
+      { include: [{model: Category},{model: Tag}] });
+  
+    if(singleProduct) {
+      res.status(200).json(singleProduct);
+    } else {
+      res.status(400).json('No Data found');
+    }
+  } catch (err) {
+      res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -53,7 +77,7 @@ router.put('/:id', (req, res) => {
   Product.update(req.body, {
     where: {
       id: req.params.id,
-    },
+    }
   })
     .then((product) => {
       // find all associated tags from ProductTag
